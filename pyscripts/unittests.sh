6a214a790633796a7c6cb48844a4f1a2a1140712
termninja <nmicovic@outlook.com>
2016-03-29 18:32:29 +0200

    added unit tests

---
+++ pyscripts/unittests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+./pygraph.py 1
+./pygraph.py 2
+./pygraph.py 3
+./pygraph.py 4
+./pygraph.py 5
+./pygraph.py 6 
+./pygraph.py 7
+./pygraph.py 8
+./pygraph.py 9
+./pygraph.py 10
+./pygraph.py 11
+./pygraph.py 12
+./pygraph.py 13
